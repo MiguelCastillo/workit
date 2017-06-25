9f19a8327b1954cdb0be21cf4d14ac81e6681303
Miguel Castillo <mcastillo@symphono.com>
2017-06-25 12:05:49 -0400

    moved worker initialization call to the worker itself instead of the pool

--- src/worker.js
+++ src/worker.js
@@ -24,12 +24,30 @@ class Worker {
     return this.pool._queueMessage(type, data, this);
   }
 
+  start(file) {
+    this.send("__init", file).catch(error => {
+      this.kill();
+
+      this.messageQueue
+        .splice(0)
+        .forEach(envelope => envelope.reject("Unable to initialize worker.\n" + error));
+
+      var pool = this.pool;
+
+      if (!pool.workers.length && pool.messageQueue.length) {
+        pool.messageQueue
+          .splice(0)
+          .forEach(envelope => envelope.reject("Unable to initialize worker.\n" + error));
+      }
+    });
+  }
+
   kill() {
     var pool = this.pool;
-    var index = pool.procs.indexOf(this);
+    var index = pool.workers.indexOf(this);
 
     if (index !== -1) {
-      pool.procs.splice(index, 1);
+      pool.workers.splice(index, 1);
       this.state = States.stopped;
       this.handle.disconnect();
     }
