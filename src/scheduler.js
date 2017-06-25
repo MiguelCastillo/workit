3afa4ab12b533f06156d1cc1bf6e4dbf2837ebfb
Miguel Castillo <mcastillo@symphono.com>
2017-06-25 13:25:34 -0400

    Introduced a scheduler to encapsulate the strategy for distributing work load

---
+++ src/scheduler.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var States = require("./states");
+
+class Scheduler {
+  constructor(pool) {
+    this.id = 1;
+    this.pool = pool;
+    this.messageQueue = [];
+  }
+
+  queue(type, data, worker) {
+    return new Promise((resolve, reject) => {
+      var id = this.id++;
+      var messageQueue = worker ? worker.messageQueue : this.messageQueue;
+
+      messageQueue.push({
+        message: {
+          id: id,
+          type: type,
+          data: data
+        },
+        resolve: resolve,
+        reject: reject
+      });
+
+      this._processNextMessage(worker);
+    })
+    .then((result) => {
+      this._processNextMessage(worker);
+      return result;
+    });
+  }
+
+  _processNextMessage(worker) {
+    var pool = this.pool;
+    var availableWorker, messageQueue;
+
+    if (worker && worker.handle.connected && worker.state === States.stopped && !worker.messageQueue.length) {
+      worker.stop();
+    }
+
+    if (worker && worker.state === States.available && worker.messageQueue.length) {
+      availableWorker = worker;
+      messageQueue = worker.messageQueue;
+    }
+    else {
+      availableWorker = pool.workers.find((worker) => worker.state === States.available);
+      messageQueue = this.messageQueue;
+    }
+
+    if (availableWorker && messageQueue.length) {
+      availableWorker._sendMessage(messageQueue.shift()); // FILO
+    }
+  }
+}
+
+module.exports = Scheduler;
--- src/worker.js
+++ src/worker.js
@@ -15,18 +15,21 @@ class Worker {
     options = options || {};
     this.settings = Object.assign({}, defaults, options);
     this.pool = pool;
+    this.pending = {};
     this.messageQueue = [];
     this.state = States.available;
     this.handle = childProcess.fork(path.join(__dirname, "./worker-process.js"), [], this.settings);
   }
 
   send(type, data) {
-    return this.pool._queueMessage(type, data, this);
+    return this.pool.scheduler.queue(type, data, this);
   }
 
   start(file) {
-    this.send("__init", file).catch(error => {
-      this.kill();
+    registerHandlers(this);
+
+    return this.send("__init", file).catch(error => {
+      this.stop();
 
       this.messageQueue
         .splice(0)
@@ -52,6 +55,42 @@ class Worker {
       this.handle.disconnect();
     }
   }
+
+  _sendMessage(envelope) {
+    this.pending[envelope.message.id] = envelope;
+    this.state = States.executing;
+    this.handle.send(envelope.message);
+  }
+}
+
+function registerHandlers(worker) {
+  worker.handle
+    .on("error", (error) => {
+      process.stderr.write(`===> process error [${worker.handle.pid}]` + error + "\n");
+    })
+    .on("message", (message) => {
+      if (worker.pending.hasOwnProperty(message.id)) {
+        worker.state = worker.state === States.executing ? States.available : worker.state;
+        handleResult(message, worker.pending[message.id]);
+        delete worker.pending[message.id];
+      }
+      else if (typeof worker.pool.settings[message.type] === "function") {
+        if (message.id) {
+          Promise.resolve(worker.pool.settings[message.type](message.data))
+            .then(data => worker.handle.send({ id: message.id, data: data }))
+            .catch(error => worker.handle.send({ id: message.id, error: error }));
+        }
+        else {
+          worker.pool.settings[message.type](message.data);
+        }
+      }
+    });
+}
+
+function handleResult(message, pending) {
+  message.error ?
+    pending.reject(message.error) :
+    pending.resolve(message.data);
 }
 
 module.exports = Worker;
