2db978d07af77d61232a8ba78eb7bf874666ac00
Miguel Castillo <mcastillo@symphono.com>
2017-06-25 11:50:17 -0400

    renamed proc to worker to be more consistent and idiomatic

--- src/pool.js
+++ src/pool.js
@@ -14,7 +14,7 @@ class Pool {
     this.id = 1;
     this.pending = {};
     this.messageQueue = [];
-    this.procs = [];
+    this.workers = [];
     this.size(size);
   }
 
@@ -23,11 +23,11 @@ class Pool {
   }
 
   kill() {
-    this.procs.forEach((proc) => proc.kill());
+    this.workers.forEach((worker) => worker.kill());
   }
 
   size(size) {
-    var currentSize = this.procs.length;
+    var currentSize = this.workers.length;
 
     if (size > currentSize) {
       this._add(size - currentSize);
@@ -38,14 +38,14 @@ class Pool {
   }
 
   _add(count) {
-    var procs = Array.apply(null, Array(count)).map(() => new Worker(this, this.settings));
+    var workers = Array.apply(null, Array(count)).map(() => new Worker(this, this.settings));
 
-    procs.forEach(proc => {
-      registerProcHandlers(this, proc);
-      initProcess(this, proc, this.file);
+    workers.forEach(worker => {
+      registerProcHandlers(this, worker);
+      initProcess(this, worker, this.file);
     });
 
-    this.procs = this.procs.concat(procs);
+    this.workers = this.workers.concat(workers);
   }
 
   _remove(count) {
@@ -53,19 +53,19 @@ class Pool {
       throw new Error("Number of items to be removed must be greater than 0");
     }
 
-    this.procs
+    this.workers
       .sort((a, b) => (
         a.state === States.available ? -1 :
         a.messageQueue.length < b.messageQueue.length ? -1 : 1
       ))
       .slice(0, count)
-      .forEach(proc => proc.kill());
+      .forEach(worker => worker.kill());
   }
 
-  _queueMessage(type, data, proc) {
+  _queueMessage(type, data, worker) {
     return new Promise((resolve, reject) => {
       var id = this.id++;
-      var messageQueue = proc ? proc.messageQueue : this.messageQueue;
+      var messageQueue = worker ? worker.messageQueue : this.messageQueue;
 
       messageQueue.push({
         message: {
@@ -77,71 +77,71 @@ class Pool {
         reject: reject
       });
 
-      processNextMessage(this, proc);
+      processNextMessage(this, worker);
     })
     .then((result) => {
-      processNextMessage(this, proc);
+      processNextMessage(this, worker);
       return result;
     });
   }
 }
 
-function processNextMessage(pool, proc) {
-  var availableProc, messageQueue;
+function processNextMessage(pool, worker) {
+  var availableWorker, messageQueue;
 
-  if (proc && proc.handle.connected && proc.state === States.stopped && !proc.messageQueue.length) {
-    proc.kill();
+  if (worker && worker.handle.connected && worker.state === States.stopped && !worker.messageQueue.length) {
+    worker.kill();
   }
 
-  if (proc && proc.state === States.available && proc.messageQueue.length) {
-    availableProc = proc;
-    messageQueue = proc.messageQueue;
+  if (worker && worker.state === States.available && worker.messageQueue.length) {
+    availableWorker = worker;
+    messageQueue = worker.messageQueue;
   }
   else {
-    availableProc = pool.procs.find((proc) => proc.state === States.available);
+    availableWorker = pool.workers.find((worker) => worker.state === States.available);
     messageQueue = pool.messageQueue;
   }
 
-  if (availableProc && messageQueue.length) {
+  if (availableWorker && messageQueue.length) {
     var envelope = messageQueue.shift(); // FILO
     pool.pending[envelope.message.id] = envelope;
-    availableProc.state = States.executing;
-    availableProc.handle.send(envelope.message);
+    availableWorker.state = States.executing;
+    availableWorker.handle.send(envelope.message);
   }
 }
 
-function initProcess(pool, proc, file) {
-  proc.send("__init", file).catch(error => {
-    proc.kill();
+function initProcess(pool, worker, file) {
+  worker.send("__init", file).catch(error => {
+    worker.kill();
 
-    proc.messageQueue
+    worker.messageQueue
       .splice(0)
-      .forEach(envelope => envelope.reject("Unable to initialize child process.\n" + error));
+      .forEach(envelope => envelope.reject("Unable to initialize worker.\n" + error));
 
-    if (!pool.procs.length && pool.messageQueue.length) {
+    if (!pool.workers.length && pool.messageQueue.length) {
       pool.messageQueue
         .splice(0)
-        .forEach(envelope => envelope.reject("Unable to initialize child process.\n" + error));
+        .forEach(envelope => envelope.reject("Unable to initialize worker.\n" + error));
     }
   });
 }
 
-function registerProcHandlers(pool, proc) {
-  proc.handle
+function registerProcHandlers(pool, worker) {
+  worker.handle
     .on("error", (error) => {
-      process.stderr.write(`===> process error [${proc.handle.pid}]` + error + "\n");
+      process.stderr.write(`===> process error [${worker.handle.pid}]` + error + "\n");
     })
     .on("message", (message) => {
       if (pool.pending.hasOwnProperty(message.id)) {
-        proc.state = proc.state === States.executing ? States.available : proc.state;
-        handleResult(message, pool.pending[message.id], proc);
+        worker.state = worker.state === States.executing ? States.available : worker.state;
+        handleResult(message, pool.pending[message.id], worker);
         delete pool.pending[message.id];
       }
       else if (typeof pool.settings[message.type] === "function") {
         if (message.id) {
           Promise.resolve(pool.settings[message.type](message.data))
-            .then(data => proc.handle.send({ id: message.id, data: data }))
-            .catch(error => proc.handle.send({ id: message.id, error: error }));
+            .then(data => worker.handle.send({ id: message.id, data: data }))
+            .catch(error => worker.handle.send({ id: message.id, error: error }));
         }
         else {
           pool.settings[message.type](message.data);
