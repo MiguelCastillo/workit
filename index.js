6e80a051b8895051d719942f5b15f420a3916145
Miguel Castillo <mcastillo@symphono.com>
2017-08-22 11:51:40 -0400

    changed api to export Pool as a named export instead of default

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-module.exports = require("./src/pool");
-module.exports.WorkerApi = require("./src/worker/api");
+module.exports = {};
+module.exports.Pool = require("./src/pool");
+module.exports.Worker = require("./src/worker/api");
 module.exports.isWorker = process.isWorker;
